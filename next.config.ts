5f5838be9bf3bdbbf677050bc11e30c565a805dd
pedrom2002 <pedrom02.dev@gmail.com>
2026-05-16 22:27:02 +0100

    chore: enable Sentry source maps, add openGraph metadata

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,6 @@ const nextConfig: NextConfig = {
 
 export default withSentryConfig(nextConfig, {
   silent: true,
-  sourcemaps: { disable: true },
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
 })
